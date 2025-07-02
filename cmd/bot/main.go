96f1d97ae6a17e20fbf404fcfe42247ea3007bfc
Matthew Song <matthewsong18@gmail.com>
2025-07-02 13:24:07 -0400

    feat(bot): added bot startup & discord connection

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -5,20 +5,58 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+
+	"github.com/bwmarrin/discordgo"
 )
 
+// Bot parameters
+var (
+	GuildID       = os.Getenv("GUILD_ID")
+	BotToken      = os.Getenv("BOT_TOKEN")
+	AppID         = os.Getenv("APP_ID")
+	DbPath        = os.Getenv("DB_PATH")
+	EncryptionKey = os.Getenv("ENCRYPTION_KEY")
+)
+
+var discordSession *discordgo.Session
+
+func init() {
+	var err error
+	discordSession, err = discordgo.New("Bot " + BotToken)
+	if err != nil {
+		log.Fatalf("Invalid bot parameters: %v", err)
+	}
+}
+
 func run() (err error) {
-	dbPath := os.Getenv("DB_PATH")
-	if dbPath == "" {
-		log.Fatal("DB_PATH environment variable is not set")
+	flagErr := false
+	if GuildID == "" {
+		log.Println("GUILD_ID environment variable is not set")
+		flagErr = true
+	}
+	if BotToken == "" {
+		log.Println("BOT_TOKEN environment variable is not set")
+		flagErr = true
+	}
+	if AppID == "" {
+		log.Println("APP_ID environment variable is not set")
+		flagErr = true
+	}
+	if DbPath == "" {
+		log.Println("DB_PATH environment variable is not set")
+		flagErr = true
 	}
 
-	encryptionKey := os.Getenv("ENCRYPTION_KEY")
-	if encryptionKey == "" {
+	if flagErr {
+		return fmt.Errorf("invalid environment variables")
+	}
+
+	if EncryptionKey == "" {
 		log.Println("ENCRYPTION_KEY environment variable is not set, using unencrypted database")
 	}
 
-	db, initDbError := storage.InitializeDatabase(dbPath, encryptionKey)
+	db, initDbError := storage.InitializeDatabase(DbPath, EncryptionKey)
 
 	if initDbError != nil {
 		return fmt.Errorf("failed to initialize database: %w", initDbError)
@@ -29,8 +67,22 @@ func run() (err error) {
 	// Initialize internal services here
 
 	// Run discord logic here
+	discordSession.AddHandler(func(discordSession *discordgo.Session, ready *discordgo.Ready) {
+		log.Println("Bot is up")
+	})
 
-	log.Println("Application started successfully")
+	err = discordSession.Open()
+	if err != nil {
+		log.Fatalf("Cannot open the session: %v", err)
+	}
+	defer func(discordSession *discordgo.Session) {
+		_ = discordSession.Close()
+	}(discordSession)
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+	log.Println("Graceful shutdown")
 
 	defer func() {
 		if closeError := db.Close(); closeError != nil {
--- go.mod
+++ go.mod
@@ -3,12 +3,16 @@ module betting-discord-bot
 go 1.24
 
 require (
+	github.com/bwmarrin/discordgo v0.29.0
 	github.com/google/uuid v1.6.0
 	github.com/tursodatabase/go-libsql v0.0.0-20250609073118-9c24e0e7fa97
 )
 
 require (
 	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/libsql/sqlite-antlr4-parser v0.0.0-20240327125255-dbf53b6cbf06 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/exp v0.0.0-20230515195305-f3d0a9c9a5cc // indirect
+	golang.org/x/sys v0.1.0 // indirect
 )
